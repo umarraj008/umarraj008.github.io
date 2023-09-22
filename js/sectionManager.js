aee3f28a9dafb5f56fb0e4bbaa9bc35df5eab27b
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2023-09-22 13:37:15 +0100

    section scroll
    - updated header with new styling
    - added sections to main body
    - sections scroll with header buttons
    - added new script to handle section scroll

---
+++ js/sectionManager.js
@@ -0,0 +1,3 @@
+function changeSection(section) {
+    document.getElementById("contentWrapper").style.transform = "translateX(" + (-100*section) + "%)";
+}
--- js/typingAnimation.js
+++ js/typingAnimation.js
@@ -1,29 +1,43 @@
-var i = 0;
-var text = "a Software Developer";
+var charCounter = 0;
+var textIndex = 0;
+var text = [
+    " Software Developer", 
+    " Java Developer", 
+    " Web Developer", 
+    " Full Stack Developer"
+];
+
 function type() {
-    if (i < text.length) {
-        document.getElementById("typing-animation").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(type, 100);
+    if (charCounter < text[textIndex].length) {
+        document.getElementById("typing-animation").innerHTML += text[textIndex].charAt(charCounter);
+        charCounter++;
+        setTimeout(type, getRandomArbitrary(10,100));
     } else {
         clearTimeout(type);
         setTimeout(function() {
-            i = document.getElementById("typing-animation").innerHTML.length;
+            charCounter = document.getElementById("typing-animation").innerHTML.length;
             untype();
-        }, 10000);
+        }, 7000);
     }
 }
 
 function untype() {
-    if (i > 0) {
+    if (charCounter > 1) {
         document.getElementById("typing-animation").innerHTML = (document.getElementById("typing-animation").innerHTML).substring(0, document.getElementById("typing-animation").innerHTML.length-1);
-        i--;
-        setTimeout(untype, 100);
+        charCounter--;
+        setTimeout(untype, getRandomArbitrary(10,100));
     } else {
         clearTimeout(untype);
-        i = 0;
+        charCounter = 0;
+        textIndex++;
+        if (textIndex >= text.length) textIndex = 0;
         type();
     }
+}
+
+// https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 setTimeout(function() {type();}, 1000);
