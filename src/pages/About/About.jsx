22c411def66e0fff55844dac484475c10e8896fd
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2024-10-29 13:51:52 +0000

    about section info
    - added image and text to about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import "./About.css";
 
 function About() {
+  /**
+   * Biography: A short paragraph about yourself, including your background, interests, and what led you to software development.
+   * Education: Briefly list relevant degrees or certifications.
+   * Experience: Summarize your professional journey, highlighting key roles and responsibilities.
+   * Personal Touch: Include hobbies or interests outside of programming that reflect your personality.
+   */
+  
   return (
-    <section id="about" className='about-container'>
+    <section id="about">
       <h1>About Me</h1>
+      <div className="info-container">
+        <div className="left-section">
+          <img src="/umar.jpg" />
+        </div>
+        <div className="right-section">
+          <p>Passionate and dedicated professional in programming, software development, and computer science. Experienced in creating innovative solutions with a strong foundation in languages and frameworks. Committed to delivering high-quality software applications, continuously expanding knowledge, and staying updated on industry trends. Possesses problem-solving and analytical skills, approaches challenges logically, and enjoys collaborating in team environments. Committed to lifelong learning and actively seeks opportunities to refine skills.</p>
+          <br />
+          <p>In my free time, I love to immerse myself in programming and exploring new technologies, constantly seeking to expand my skill set. Gaming is another passion of mine, providing both entertainment and inspiration for my projects.</p>
+        </div>
+      </div>
     </section>
   )
 }
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import "./Contact.css";
 
 function Contact() {
+  /**
+   * Contact Form: Include a simple form for visitors to get in touch (Name, Email, Message).
+   * Email Address: Provide your email for direct contact.
+   * Social Links: Links to your LinkedIn, GitHub, Twitter, or any other relevant profiles.
+   */
+  
   return (
     <section id="contact">
       <div className='contact-container'>
