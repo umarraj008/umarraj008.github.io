d539b583b12ccac21ae15f5c42461f3731ccb981
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2024-11-07 23:09:52 +0000

    fixing cv download

--- src/pages/CV/CV.jsx
+++ src/pages/CV/CV.jsx
@@ -6,7 +6,7 @@ function CV() {
     <section id="cv">
       <h1>CV</h1>
       <div className="cv-container">
-        <h2><a href="/Umar Rajput CV 2024 v4.pdf"><span className="material-symbols-outlined">download</span>&nbsp;Download my CV</a></h2>
+        <h2><a href="/UmarRajputCV2024v4.pdf"><span className="material-symbols-outlined">download</span>&nbsp;Download my CV</a></h2>
       </div>
     </section>
   )
