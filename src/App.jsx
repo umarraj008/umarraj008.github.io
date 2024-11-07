280b97d10287ff623cb60e300238ccc9b62c323a
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2024-11-07 23:00:06 +0000

    updating threshold

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import ThemeChangeButton from './components/ThemeChangeButton/ThemeChangeButton';
 
-const threshold = 0.6;
+const threshold = 0.4;
 
 function App() {
   const [currentSection, setCurrentSection] = useState("");
